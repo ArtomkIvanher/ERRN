022701d96b990e58ed5e01bf259c7c3fb9eb7352
ARTOMK <artomk909@gmail.com>
2024-12-29 14:18:59 +0200

    провірка

--- components/pages/main/Home/Home.jsx
+++ components/pages/main/Home/Home.jsx
@@ -104,7 +104,7 @@ export default function Home() {
   const handleSignOut = async () => {
     try {
       await signOut(auth);
-      console.log('Вихід виконано успішно   ааццаааааа');
+      console.log('Вихід виконано успішно');
     } catch (error) {
       console.error('Помилка виходу:', error.message);
     }
