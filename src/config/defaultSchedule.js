dff73371b16c2cb9549cf4f2900e4e9158cd19a6
ARTOMK <artomk909@gmail.com>
2025-01-15 12:16:53 +0200

    defaultSchedule

---
+++ src/config/defaultSchedule.js
@@ -0,0 +1,69 @@
+const defaultSchedule = {
+	duration: 80,
+	breaks: [10, 20, 10, 10],
+	start_time: '08:30',
+	auto_save: 60,
+	repeat: 1,
+	subjects: [
+		{
+			id: 1,
+			name: 'Mathematics',
+			teacher: 2,
+			zoom_link: 'https://zoom.com/lesson1',
+		},
+		{
+			id: 2,
+			name: 'Ukrainian Language',
+			teacher: 1,
+			zoom_link: 'https://zoom.com/lesson2',
+		},
+		{
+			id: 3,
+			name: 'Biology',
+			teacher: 4,
+			zoom_link: 'https://zoom.com/lesson3',
+		},
+		{
+			id: 4,
+			name: 'Physics',
+			teacher: 5,
+			zoom_link: 'https://zoom.com/lesson4',
+		},
+		{
+			id: 5,
+			name: 'Informatics',
+			teacher: 3,
+			zoom_link: 'https://zoom.com/lesson5',
+		},
+	],
+	teachers: [
+		{
+			id: 1,
+			name: 'John Doe',
+			phone: '09340001',
+		},
+		{
+			id: 2,
+			name: 'Jane Smith',
+			phone: '09340002',
+		},
+		{
+			id: 3,
+			name: 'Mark Brown',
+			phone: '09340003',
+		},
+		{
+			id: 4,
+			name: 'Emily White',
+			phone: '09340004',
+		},
+		{
+			id: 5,
+			name: 'Alice Green',
+			phone: '09340005',
+		},
+	],
+	schedule: Array(7).fill({ week1: [], week2: [], week3: [], week4: [] }),
+}
+
+export default defaultSchedule
--- src/pages/ScheduleSettings/components/ResetDB.jsx
+++ src/pages/ScheduleSettings/components/ResetDB.jsx
@@ -1,53 +1,41 @@
-import React from "react";
-import { Alert, Button, StyleSheet, View } from "react-native";
-import { auth, db } from "../../../../firebase"; // Firebase auth і Firestore
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc } from 'firebase/firestore'
+import React from 'react'
+import { Alert, Button, StyleSheet, View } from 'react-native'
+import { auth, db } from '../../../../firebase' // Firebase auth і Firestore
+import defaultSchedule from '../../../config/defaultSchedule'
 
 export default function ResetDB() {
-  const resetFirestore = async () => {
-    const user = auth.currentUser;
-    if (!user) {
-      Alert.alert("Помилка", "Будь ласка, увійдіть у свій акаунт.");
-      return;
-    }
+	const resetFirestore = async () => {
+		const user = auth.currentUser
+		if (!user) {
+			Alert.alert('Помилка', 'Будь ласка, увійдіть у свій акаунт.')
+			return
+		}
 
-    try {
-      const scheduleRef = doc(db, "schedules", user.uid);
-      const newScheduleData = {
-        schedule: {
-          duration: 80,
-          breaks: [10, 20, 10, 10],
-          start_time: "08:30",
-          auto_save: 60,
-          repeat: 1,
-          subjects: [
-            { id: 1, name: "Mathematics", teacher: "John Doe", zoom_link: "https://zoom.com/lesson1" },
-            { id: 2, name: "Ukrainian Language", teacher: "Jane Smith", zoom_link: "https://zoom.com/lesson2" },
-            { id: 3, name: "Biology", teacher: "Mark Brown", zoom_link: "https://zoom.com/lesson3" },
-            { id: 4, name: "Physics", teacher: "Emily White", zoom_link: "https://zoom.com/lesson4" },
-            { id: 5, name: "Informatics", teacher: "Alice Green", zoom_link: "https://zoom.com/lesson5" },
-          ],
-          schedule: Array(7).fill({ week1: [], week2: [], week3: [], week4: [] }),
-        },
-      };
+		try {
+			const scheduleRef = doc(db, 'schedules', user.uid)
 
-      await setDoc(scheduleRef, newScheduleData);
-      Alert.alert("Успіх", "Дані успішно оновлено в Firestore!");
-    } catch (error) {
-      console.error("Помилка при оновленні Firestore:", error);
-      Alert.alert("Помилка", "Сталася помилка. Спробуйте ще раз.");
-    }
-  };
+			await setDoc(scheduleRef, { schedule: defaultSchedule })
+			Alert.alert('Успіх', 'Дані успішно оновлено в Firestore!')
+		} catch (error) {
+			console.error('Помилка при оновленні Firestore:', error)
+			Alert.alert('Помилка', 'Сталася помилка. Спробуйте ще раз.')
+		}
+	}
 
-  return (
-    <View style={styles.container}>
-      <Button title="Оновити дані у Firestore" onPress={resetFirestore} color="red" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Button
+				title='Оновити дані у Firestore'
+				onPress={resetFirestore}
+				color='red'
+			/>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 16,
-  },
-});
+	container: {
+		padding: 16,
+	},
+})
